6f595df56739cd42063a0e4ed6dbcef80ca0aabc
Irina <149883699+irinakravtsova@users.noreply.github.com>
2024-06-06 14:22:58 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Heading from './components/Heading/Heading'
+import TripsSection from './components/TripsSection/TripsSection'
+
+
+function App() {
+ 
+  return (
+    <div className='container'>   
+      <Heading />
+      <TripsSection />
+    </div>
+ 
+  )
+}
+
+export default App
---
+++ components/Heading/Heading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Heading.css'
+
+
+function Heading() {
+ 
+  return (
+    <h1 className='heading-h1' >Галерея путешествий</h1>
+  );
+
+}
+
+export default Heading;
